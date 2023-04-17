1cd2fb5d8fe5b1b665436bfb28658507aba3f17c
impaseri <123526115+impaseri@users.noreply.github.com>
2023-04-17 17:47:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 import Modal from './Component/Modal';
 
 function App() {
-  let [a, setA] = useState(['블로그 글 제목1','블로그 글 제목2','블로그 글 제목3']);
-  let [like, setLike] = useState([0, 0, 0]);
+  let [a, setA] = useState(['누구보다 빠르게 집에 가기','멋지게 발표하기','끝내주게 숨쉬기','울면서 이력서 쓰기', '배부르게 먹기']);
+  let [like, setLike] = useState([0, 0, 0, 0, 0]);
   let [modal, setModal] = useState(false);
+  // let [black, setblack] = useState(false);
   let [title, setTitle] = useState(0);
   let [input, setInput] = useState('');
+  let date = new Date();
+
+  let year = date.getFullYear();
+  let month = ('0' + (date.getMonth() + 1)).slice(-2);
+  let day = ('0' + date.getDate()).slice(-2);
+  let dateStr = year + '-' + month + '-' + day;
 
   let write =  function(){
     if(input == '' || input == null || input == undefined){
@@ -23,43 +30,48 @@ function App() {
     }
   }
 
+  
+
 
   return (
     <div className="App">
-      <div className='black-nav'>
         <h4>I CAN (TO) DO IT</h4>
-      </div>
-      {
-        a.map(function(parm, i){
-          return (
-            <div className='list'>
-              <h4 onClick={()=>{
-                setTitle(i);
-                setModal(modal == true ? modal = false : modal = true)
-              }}>{a[i]}</h4><span onClick={()=>{
-                let copyLike = [...like];
-                copyLike[i] = copyLike[i] + 1;
-                setLike(copyLike)}}>😎</span> {like[i]}
-              {/* <p>글쓴이 : 주인장</p> */}
-              <button onClick={()=>{
-                let copyA2 = [...a];
-                copyA2.splice(i, 1);
-                setA(copyA2);
-              }}>Delete</button>
-            </div>
-          )
-        })
-      }
-      <div className='input'>
-        <h4>New</h4>
-        <input type="text"  onChange={(e)=>{
-          setInput(e.target.value);
-        }}/>
-        <button onClick={write}>글쓰기</button>
-        {
-          modal == true ? <Modal color="yellow" name={a} title={title} /> : null
+        <div className='inner'>
+          {
+          a.map(function(parm, i){
+            return (
+              <div className='list'>
+                <div className='flex'>
+                  <h4 onClick={()=>{
+                    setTitle(i);
+                    setModal(modal == true ? modal = false : modal = true);
+                    // setblack(black == true ? black = false : black = true);
+                  }}>{a[i]}</h4>
+                  <button onClick={()=>{
+                  let copyA2 = [...a];
+                  copyA2.splice(i, 1);
+                  setA(copyA2);
+                }}>Clear</button>
+                </div>
+                <span className='drag' onClick={()=>{
+                  let copyLike = [...like];
+                  copyLike[i] = copyLike[i] + 1;
+                  setLike(copyLike)}}>응급💨</span> {like[i]}
+              </div>
+            )
+          })
         }
       </div>
+      <div className='input'>
+        <input type="text" autoFocus onChange={(e)=>{
+          setInput(e.target.value);
+        }}/>
+        <button onClick={write}>Add</button>
+
+      </div>
+      {
+          modal == true ? <Modal name={a} title={title} date={dateStr} /> : null
+        }
     </div>
   );
 }
